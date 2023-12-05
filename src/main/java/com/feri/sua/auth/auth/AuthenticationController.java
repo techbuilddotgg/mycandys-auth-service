bbb2c64f2a8d65316aede666771b0420693e7d45
Matevz <semprimoznik.matevz@gmail.com>
2023-12-05 11:16:09 +0100

    feat: add logout route

--- src/main/java/com/feri/sua/auth/auth/AuthenticationController.java
+++ src/main/java/com/feri/sua/auth/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.feri.sua.auth.auth;
 
 import com.feri.sua.auth.auth.dto.VerifyTokenResponseDto;
+import com.feri.sua.auth.config.LogoutService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ import java.io.IOException;
 public class AuthenticationController {
 
   private final AuthenticationService service;
+  private final LogoutService logoutService;
 
   @PostMapping("/register")
   public ResponseEntity<AuthenticationResponse> register(
@@ -46,6 +48,15 @@ public class AuthenticationController {
       HttpServletResponse response
   ) throws IOException {
     return service.refreshToken(request, response);
+  }
+
+  @SecurityRequirement(name = "Bearer Authentication")
+  @PostMapping("/logout")
+  public void logout(
+      HttpServletRequest request,
+      HttpServletResponse response
+  ) throws IOException {
+    logoutService.logout(request, response, null);
   }
 
 

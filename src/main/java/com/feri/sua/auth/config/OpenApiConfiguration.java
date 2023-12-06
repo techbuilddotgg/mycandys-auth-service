c21140b5e80e388c81852ffe16c4fd90b65eb36b
Matevz <semprimoznik.matevz@gmail.com>
2023-12-06 10:40:04 +0100

    fix: swagger servers

--- src/main/java/com/feri/sua/auth/config/OpenApiConfiguration.java
+++ src/main/java/com/feri/sua/auth/config/OpenApiConfiguration.java
@@ -1,33 +1,59 @@
 package com.feri.sua.auth.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                description = "Authentification API for MyCandys application",
-                title = "MyCandys Authentification API",
-                version = "1.0"
-        ),
-        servers = {
-                @Server(
-                        url = "http://localhost:8080"
-                ),
+@Configuration
+public class OpenApiConfiguration {
+
+        @Value("${application.swagger.server.url}")
+        private String swaggerServerUrl;
+
+        @Value("${server.port}")
+        private String serverPort;
+
+        @Value("${spring.profiles.active}")
+        private String activeProfile;
+
+        @Bean
+        public OpenAPI defineOpenApi() {
+                return new OpenAPI()
+                        .info(defineInfo())
+                        .addServersItem(defineServer())
+                        .components(new Components().addSecuritySchemes("Bearer Authentication", defineSecurityScheme()));
         }
-)
-@SecurityScheme(
-        name = "Bearer Authentication",
-        description = "JWT auth description",
-        scheme = "bearer",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        in = SecuritySchemeIn.HEADER
-)
-public class OpenApiConfiguration { }
+
+
+        private Server defineServer() {
+                if(activeProfile.equalsIgnoreCase("production") || activeProfile.equalsIgnoreCase("prod"))
+                        return new Server().url(swaggerServerUrl)
+                                .description("Default server");
+
+                return new Server().url("http://localhost:" + serverPort)
+                        .description("Default server");
+        }
+
+        private Info defineInfo() {
+                return new Info()
+                        .title("MyCandys Authentification API")
+                        .description("Authentification API for MyCandys application")
+                        .version("1.0");
+        }
+
+        private SecurityScheme defineSecurityScheme() {
+                return new SecurityScheme()
+                        .name("Bearer Authentication")
+                        .description("JWT auth description")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER);
+
+        }
+}
